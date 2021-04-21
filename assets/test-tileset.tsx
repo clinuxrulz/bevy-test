57b695d8df81d48ab82557309d455fd2545fc86b
Clinton Selke <clinuxrulz@gmail.com>
2021-04-21 17:24:09 +1000

    initial commit

---
+++ assets/test-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="test-tileset" tilewidth="32" tileheight="32" tilecount="4016" columns="8">
+ <image source="BenderWaffles_PublicOutside_Tileset/BW_PublicOutside_New.png" width="256" height="16064"/>
+</tileset>
