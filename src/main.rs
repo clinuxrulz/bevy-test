58d8b1cd62df04cbc1bc05453c9f553bf35f55b8
Clinton Selke <clinuxrulz@gmail.com>
2021-04-22 21:03:47 +1000

    wip load tileset into bevy

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,81 @@
-use bevy::prelude::*;
+use bevy::{app::CoreStage::PreUpdate, prelude::*, render::camera::Camera};
+use bevy_tiled_prototype::{MapReadyEvent, TiledMapCenter};
 
 fn main() {
-    let mut app = App::build();
-    app.insert_resource(Msaa { samples: 4 })
-        .add_plugins(DefaultPlugins);
-    #[cfg(target_arch = "wasm32")]
-    app.add_plugin(bevy_webgl2::WebGL2Plugin);
-    app.add_startup_system(setup.system()).run();
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(bevy_tiled_prototype::TiledMapPlugin)
+        .add_system(bevy::input::system::exit_on_esc_system.system())
+        .add_startup_system(setup.system())
+        .add_system(camera_movement.system())
+        // Needs to run before rendering to set texture atlas filter for new tiles- would be better to use states
+        .add_system_to_stage(PreUpdate, set_texture_filters_to_nearest.system())
+        .run();
 }
 
-/// set up a simple 3D scene
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // add entities to the world
-    // plane
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(bevy_tiled_prototype::TiledMapBundle {
+        map_asset: asset_server.load("test-map.tmx"),
+        center: TiledMapCenter(true),
+        origin: Transform::from_scale(Vec3::new(4.0, 4.0, 1.0)),
         ..Default::default()
     });
-    // cube
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
-        ..Default::default()
-    });
-    // light
-    commands.spawn_bundle(LightBundle {
-        transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
-        ..Default::default()
-    });
-    // camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_translation(Vec3::new(-2.0, 2.5, 5.0))
-            .looking_at(Vec3::default(), Vec3::Y),
-        ..Default::default()
-    });
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
+
+fn camera_movement(
+    time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Camera, &mut Transform)>,
+) {
+    for (_, mut transform) in query.iter_mut() {
+        let mut direction = Vec3::ZERO;
+        let scale = transform.scale.x;
+
+        if keyboard_input.pressed(KeyCode::A) {
+            direction -= Vec3::new(1.0, 0.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::D) {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::W) {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::S) {
+            direction -= Vec3::new(0.0, 1.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::Z) {
+            let scale = scale + 0.1;
+            transform.scale = Vec3::new(scale, scale, scale);
+        }
+
+        if keyboard_input.pressed(KeyCode::X) && scale > 1.1 {
+            let scale = scale - 0.1;
+            transform.scale = Vec3::new(scale, scale, scale);
+        }
+
+        transform.translation += time.delta_seconds() * direction * 1000.;
+    }
+}
+
+// demo of https://github.com/StarArawn/bevy_tiled/issues/47#issuecomment-817126515
+//  Would be cleaner to put this in a separate AppState, transitioning out after textures loaded
+fn set_texture_filters_to_nearest(
+    mut map_ready_events: EventReader<MapReadyEvent>,
+    mut textures: ResMut<Assets<Texture>>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+) {
+    // quick and dirty, run this for all textures every time a map is created/modified
+    if map_ready_events.iter().count() > 0 {
+        for (_, atlas) in texture_atlases.iter() {
+            if let Some(texture) = textures.get_mut(atlas.texture.clone()) {
+                texture.sampler.min_filter = bevy::render::texture::FilterMode::Nearest;
+            }
+        }
+    }
+}
+
